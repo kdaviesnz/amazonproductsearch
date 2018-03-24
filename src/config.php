48f7f6c79bd6dad57388e3c635685668972bf1cf
Kevin Davies <kdavies@gmail.com>
2018-03-25 08:15:11 +1300

    updated config

--- src/config.php
+++ src/config.php
@@ -1,30 +1,6 @@
 <?php
 
 
-if (isset($_SERVER["HTTP_HOST"]) && $_SERVER["HTTP_HOST"]=="premiumwebtechnologies.com") {
-	$conn = mysqli_connect("localhost", "kdavies_jeffw", "7Hpd9ub5", "kdavies_jeffw");
-
-} else {
-	$conn = mysqli_connect("localhost", "root", "7Hpd9ub5", "kdavies_jeffw");
-}
-
-if (!$conn) {
-	throw new Exception(("Could not connect to database"));
-}
-
-$options = array(
-	'amazon_accounts' => array(
-		array(
-			'amazon_unique_name' => 'Bob',
-			'amazon_secret_access_key' => 'Z0IMBN8mDpijLRe1sRnu/PUonU9V4RTEaFI6XyES',
-			'amazon_access_key_id' => 'AKIAIUKL35B3SHB6WUOQ',
-			'amazon_affiliate_link' => 'crosswordheav-20'
-		)
-	),
-	'cache' => true
-);
-
-/*
 $conn = mysqli_connect("localhost", "dbuser", "dbpassword", "dbname");
 
 if (!$conn) {
@@ -41,4 +17,3 @@ $options = array(
 		)
 	)
 );
-*/
